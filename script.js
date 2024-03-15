13173f7416d8cc2730515dc2e4e291404e217295
Thomas Roth <thomas.roth@aon.at>
2024-03-15 16:55:43 +0100

    Code playRound function
    Compare player and computer Selection and determine
    wether it's a tie, win or lose via multiple
    if commands

--- script.js
+++ script.js
@@ -7,13 +7,15 @@
     //in each case store a string with either "rock", "paper" or "scrisors" in another variable called computerSelection
     //return computerSelection variable
 
-//Play a round of rock paper scissors
-    //
-
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
+//Play a round of rock paper scissors by calling a function
+    //create function playRound with playerSelection and computerSelection as parameters
+    //compare playerSelection to computerSelection 
+                    //if computerSelection is rock and palyerSelection is rock => tie 
+                    //if computerSelection is rock and palyerSelection is paper => win
+                    //an so on
 
 //Have Computer pick either rock, paper or scissors and return the choice
+
 function getComputerChoice(){
 
     let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -30,4 +32,38 @@ function getComputerChoice(){
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    // if (playerSelection === "rock" || "paper" || "scissors") {
+    //     return "Type in a valid option!"
+    // }
+    if (playerSelection === computerSelection){
+        return `You both chose '${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}' It's a tie!`
+    }
+    
+    if (playerSelection === "rock") {
+        return (computerSelection === "paper") ? 
+        "'Paper' beats 'Rock'. You lose." : 
+        "'Rock' beats 'Scissors'. You win!"
+    }
+    
+    if (playerSelection === "paper") {
+        return (computerSelection === "scissors") ? 
+        "'Scissors' beats 'Paper'. You lose." : 
+        "'Paper' beats 'Rock'. You win!"
+    }
+    
+    if (playerSelection === "scissors") {
+        return (computerSelection === "rock") ? 
+        "'Rock' beats 'Scissors'. You lose." : 
+        "'Scissors' beats 'Paper'. You win!"
+    }
+  }
+  
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
+console.log("You chose: "+ playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
+console.log("Computer chose: " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1));
+console.log(playRound(playerSelection, computerSelection));
+
+  
 //console.log(playerSelection);
