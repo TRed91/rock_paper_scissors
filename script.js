d71012d9c2f77b879d018f8b159f2052e6a41704
Thomas Roth <thomas.roth@aon.at>
2024-03-15 17:52:57 +0100

    Add winCounter function and typo fix
    Fixed a typo where computer would choose "scrissors"
    Added winCounter variables for computer and player
    and finally compared them in a function to determine
    a winner

--- script.js
+++ script.js
@@ -31,42 +31,58 @@ function getComputerChoice(){
         return "paper";
     
         default:
-        return "scrissors";
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-     if (playerSelection === "rock" || "paper" || "scissors") {
+
+     if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
         
         if (playerSelection === computerSelection){
             return `You both chose '${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}' It's a tie!`;
         }
         
         if (playerSelection === "rock") {
-            return (computerSelection === "paper") ? 
-            "'Paper' beats 'Rock'. You lose." : 
-            "'Rock' beats 'Scissors'. You win!";
+            if (computerSelection === "paper") {
+                winCounterComputer += 1;
+               return "'Paper' beats 'Rock'. You lose.";
+            } else {
+                winCounterPlayer += 1;
+                return "'Rock' beats 'Scissors'. You win!";
+            }
         }
         
         if (playerSelection === "paper") {
-            return (computerSelection === "scissors") ? 
-            "'Scissors' beats 'Paper'. You lose." : 
-            "'Paper' beats 'Rock'. You win!";
+            if (computerSelection === "scissors") {
+                winCounterComputer += 1;
+                return "'Scissors' beats 'Paper'. You lose.";
+             } else {
+                winCounterPlayer += 1;
+                return "'Paper' beats 'Rock'. You win!";
+            }
         }
         
         if (playerSelection === "scissors") {
-            return (computerSelection === "rock") ? 
-            "'Rock' beats 'Scissors'. You lose." : 
-            "'Scissors' beats 'Paper'. You win!";
+            if (computerSelection === "rock") { 
+                winCounterComputer += 1;
+                return "'Rock' beats 'Scissors'. You lose." ; 
+            } else {
+                winCounterPlayer += 1;
+                return "'Scissors' beats 'Paper'. You win!";
+            }
         }
       
      } else {
-        return "Type in a valid option!";
+        winCounterComputer += 1;
+        return "Can't even type Rock, Paper or Scissors eh? Computer wins.";
      }
     }
   
     function playGame() {
+
         for (let i = 0; i < 5; i++){
+
             const computerSelection = getComputerChoice();
             const playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
             playRound(playerSelection, computerSelection);
@@ -75,6 +91,19 @@ function playRound(playerSelection, computerSelection) {
             console.log(playRound(playerSelection, computerSelection));
         }
     }
-  
+
+    function compareCounters(winCounterPlayer, winCounterComputer) {
+        if (winCounterComputer === winCounterPlayer) {
+            return "It's a tie";
+        } else if (winCounterPlayer > winCounterComputer) {
+            return "You win!";
+        } else {
+            return "You lose."
+        }
+    }
+
+let winCounterPlayer = 0;
+let winCounterComputer = 0;
 
 playGame();
+console.log(compareCounters(winCounterPlayer, winCounterComputer));
