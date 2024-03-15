89afea0026ebc268b5b4875a5ad17ab267f82d5a
Thomas Roth <thomas.roth@aon.at>
2024-03-15 13:22:49 +0100

    create playerSelection and computerSelection
    wrote pseudocode algorythm and code for
    getting player selection via prompt and computerSelection
    via function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+// Create prompt input for player to choose either rock, paper or scrissors
+    //store the choice inside a variable = playerSelection
+
+// PC picks rock, paper or scissors in a function
+    //generate random number between 1 and 3 and store in variable
+    //create if or switch command based on the value of that variable
+    //in each case store a string with either "rock", "paper" or "scrisors" in another variable called computerSelection
+    //return computerSelection variable
+
+//Play a round of rock paper scissors
+    //
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
+
+//Have Computer pick either rock, paper or scissors and return the choice
+function getComputerChoice(){
+
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    
+    switch (randomNumber){
+        case 3:
+        return "rock";
+    
+        case 2:
+        return "paper";
+    
+        default:
+        return "scrissors";
+    }
+}
+
+//console.log(playerSelection);
