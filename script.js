f55582f9eccf2729934ddeb98ffa3dad821a711b
Thomas Roth <thomas.roth@aon.at>
2024-03-19 11:41:04 +0100

    add functions to display selections in upper Case
    added two functions so the Selections of player
    and computer are being displayed with the first
    letter being in upper Case.
    Also changed other lines to call the functions
    insted of variables

--- script.js
+++ script.js
@@ -12,21 +12,29 @@ function getComputerChoice (){
     }
   }
   
+function playerInputConversion(playerSelection) {
+  return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+}
+
+function compSelectionConv(computerSelection) {
+  return computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+}
+
   function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
-      return `You both chose ${playerSelection}. It's a tie!`;
+      return `You both chose ${playerInputConversion(playerSelection)}. It's a tie!`;
   
   
     } else if ( playerSelection === "rock" && computerSelection === "scissors" ||
                 playerSelection === "paper" && computerSelection === "rock" ||
                 playerSelection === "scissors" && computerSelection === "paper" ) {
-      return `${playerSelection} beats ${computerSelection}. Player wins!`;
+      return `${playerInputConversion(playerSelection)} beats ${compSelectionConv(computerSelection)}. Player wins!`;
   
   
     } else if ( playerSelection === "rock" && computerSelection === "paper" ||
                 playerSelection === "paper" && computerSelection === "scissors" ||
                 playerSelection === "scissors" && computerSelection === "rock" ) {
-      return `${computerSelection} beats ${playerSelection}. Computer wins.`;
+      return `${compSelectionConv(computerSelection)} beats ${playerInputConversion(playerSelection)}. Computer wins.`;
   
   
     } else {
@@ -44,14 +52,15 @@ function getComputerChoice (){
       const computerSelection = getComputerChoice();
       const result = playRound(playerSelection, computerSelection);
   
-      console.log("Player chose: " + playerSelection + ", Computer chose: " + computerSelection);
+      console.log("Player chose: " + playerInputConversion(playerSelection) + ", Computer chose: " + compSelectionConv(computerSelection));
       console.log(result);
       
-      if (result === `${playerSelection} beats ${computerSelection}. Player wins!`) {
+      if (result === `${playerInputConversion(playerSelection)} beats ${compSelectionConv(computerSelection)}. Player wins!`) {
         winCounter ++;
-      } else if (result === `${computerSelection} beats ${playerSelection}. Computer wins.`) {
+      } else if (result === `${compSelectionConv(computerSelection)} beats ${playerInputConversion(playerSelection)}. Computer wins.`) {
         winCounter --;
       }
+      console.log(winCounter);
     }
   
     if (winCounter > 0) {
