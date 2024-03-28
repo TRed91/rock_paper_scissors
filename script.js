4cdaf947391183bf364e9a566da396da5d6a6785
Thomas Roth <thomas.roth@aon.at>
2024-03-28 22:12:26 +0100

    create basic button ui
    created buttons in html and added reference
    and listener event to script. also reorganized
    some functions and removed the 'play 5 rounds'
    loop.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const btnContainer = document.querySelector("#btnContainer");
+const body = document.querySelector('body');
+
+btnContainer.addEventListener('click', playRound);
+
 function getComputerChoice (){
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     switch (randomNumber) {
@@ -11,7 +16,15 @@ function getComputerChoice (){
         return "scissors";
     }
   }
-  
+
+function getPlayerChoice (target) {
+  switch (target.id) {
+    case 'btnRock': return 'rock';
+    case 'btnPaper': return 'paper';
+    case 'btnScissors': return 'scissors';
+  }
+}
+
 function playerInputConversion(playerSelection) {
   return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 }
@@ -20,56 +33,38 @@ function compSelectionConv(computerSelection) {
   return computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
 }
 
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection){
-      return `You both chose ${playerInputConversion(playerSelection)}. It's a tie!`;
-  
-  
-    } else if ( playerSelection === "rock" && computerSelection === "scissors" ||
-                playerSelection === "paper" && computerSelection === "rock" ||
-                playerSelection === "scissors" && computerSelection === "paper" ) {
-      return `${playerInputConversion(playerSelection)} beats ${compSelectionConv(computerSelection)}. Player wins!`;
-  
-  
-    } else if ( playerSelection === "rock" && computerSelection === "paper" ||
-                playerSelection === "paper" && computerSelection === "scissors" ||
-                playerSelection === "scissors" && computerSelection === "rock" ) {
-      return `${compSelectionConv(computerSelection)} beats ${playerInputConversion(playerSelection)}. Computer wins.`;
-  
-  
-    } else {
-      return "Invalid input";
-    }
-  }
-  
-  function playGame() {
-  
-    let winCounter = 0;
-  
-    for (let i = 0; i < 5; i++) {
-      
-      const playerSelection = prompt("Pick 'Rock', 'Paper' or 'Scissors': ").toLowerCase();
-      const computerSelection = getComputerChoice();
-      const result = playRound(playerSelection, computerSelection);
-  
-      console.log("Player chose: " + playerInputConversion(playerSelection) + ", Computer chose: " + compSelectionConv(computerSelection));
-      console.log(result);
-      
-      if (result === `${playerInputConversion(playerSelection)} beats ${compSelectionConv(computerSelection)}. Player wins!`) {
-        winCounter ++;
-      } else if (result === `${compSelectionConv(computerSelection)} beats ${playerInputConversion(playerSelection)}. Computer wins.`) {
-        winCounter --;
-      }
+function resultCalc(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection){
+    return `You both chose ${playerInputConversion(playerSelection)}. It's a tie!`;
 
-    }
-  
-    if (winCounter > 0) {
-      return "Player wins!";
-    } else if (winCounter < 0) {
-      return "Computer wins!";
+
+   } else if ( playerSelection === "rock" && computerSelection === "scissors" ||
+         playerSelection === "paper" && computerSelection === "rock" ||
+         playerSelection === "scissors" && computerSelection === "paper" ) {
+     return `${playerInputConversion(playerSelection)} beats ${compSelectionConv(computerSelection)}. Player wins!`;
+
+
+   } else if ( playerSelection === "rock" && computerSelection === "paper" ||
+         playerSelection === "paper" && computerSelection === "scissors" ||
+         playerSelection === "scissors" && computerSelection === "rock" ) {
+     return `${compSelectionConv(computerSelection)} beats ${playerInputConversion(playerSelection)}. Computer wins.`;
+
     } else {
-      return "It's a tie!";
-     }
+    return "Invalid input";
+    }
+}
+
+function playRound(event) {
+    let target = event.target;
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerChoice(target);
+    const result = resultCalc(playerSelection, computerSelection);
+    const resultText = document.createElement("div");
+    resultText.textContent = result;
+    body.appendChild(resultText);
   }
   
-  console.log(playGame())
+ 
+
+  
+ 
